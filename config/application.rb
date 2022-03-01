72874383cc869a6566130f56d99e318c6c257194
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2022-02-28 23:22:36 -0300

    generate email scaffold and add rich_text

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 require_relative "boot"
 
-require "rails"
+require "rails/all"
 # Pick the frameworks you want:
 require "active_model/railtie"
 # require "active_job/railtie"
@@ -11,8 +11,6 @@ require "action_mailer/railtie"
 # require "action_mailbox/engine"
 # require "action_text/engine"
 require "action_view/railtie"
-# require "action_cable/engine"
-require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :emails
   post "welcome/unsubscribe/:user", to: "welcome#unsubscribe"
   get "welcome/index"
 
